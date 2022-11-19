6964a07355415c631bb0b2f6e5221ceb9e2c5f5f
PriscillaPeter <65596006+PriscillaPeter@users.noreply.github.com>
2022-11-18 21:52:03 -0600

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -9,12 +9,15 @@ def test_init():
 def test_deposit(): 
     account_1 = Account('Family Trust')
     assert account_1.deposit(-10) is False
+    assert account_1.deposit(0) is False
     assert account_1.deposit(10)  
     assert account_1.get_balance() == 10  
       
 def test_withdraw():
     account_1 = Account('Family Trust')
-    assert account_1.get_name() == "Family Trust"
-    account_1.deposit(10)
-    account_1.withdraw(5)
+    assert account_1.get_name() == 'Family Trust'
+    assert account_1.withdraw(-10) is False
+    assert account_1.withdraw(0) is False
+    assert account_1.deposit(10)
+    assert account_1.withdraw(5)
     assert account_1.getbalance() == 5 
